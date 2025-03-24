34bedc291cbc8519318e922cdbc19721bb84d5c9
Thiago-Morilli <thiagomorilli50@gmail.com>
2025-03-24 10:50:55 +0000

    new

--- Alura/jogoteca/Jogoteca.py
+++ Alura/jogoteca/Jogoteca.py
@@ -7,11 +7,9 @@ app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+
 from views import *
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    app.run(debug=True) 
